63a7478d4520169496282e544a420c4bf4c8e3f4
charlyrodriguesplana <crodrigues@planatechnologies.com>
2025-10-27 15:01:41 -0300

    feat: Domain Sales - Create Use case Get By Id

--- src/123Vendas.Domain.Sales.Api/Mappings/SaleProfile.cs
+++ src/123Vendas.Domain.Sales.Api/Mappings/SaleProfile.cs
@@ -5,14 +5,10 @@ using _123Vendas.Domain.Sales.Application.UsesCase.Shared;
 
 namespace _123Vendas.Domain.Sales.Api.Mappings
 {
-    /// <summary>
-    /// AutoMapper profile for Sale mappings
-    /// </summary>
     public class SaleProfile : Profile
     {
         public SaleProfile()
         {
-            // Request to Command mappings
             CreateMap<CreateSaleRequest, CreateSaleCommand>();
             CreateMap<CreateSaleItemRequest, SaleItemDto>();
         }
